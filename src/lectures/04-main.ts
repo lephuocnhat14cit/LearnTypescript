1af47aa9e4349b69c73d0bb48363e3151604fc91
lephuocnhat <lephuocnhat.14cit@gmail.com>
2021-09-19 21:39:25 +0700

    type assion

--- src/lectures/04-main.ts
+++ src/lectures/04-main.ts
@@ -2,3 +2,18 @@ export function isPositive(n:number) {
   return n > 0;
 }
 
+const count = 1;
+console.log(count);
+
+const student = {
+  id: 1,
+  name: 'nhat',
+  isActive: true
+}
+
+function getStatus (state: string)  {
+  console.log(state);
+}
+
+let s = 'active';
+getStatus(s); 
